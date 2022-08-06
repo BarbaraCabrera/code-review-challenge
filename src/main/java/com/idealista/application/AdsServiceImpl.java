542a0fa5977dc0317aca60ca9e57ccb051cd446b
BarbaraCabrera <barbara.cabrera@liferay.com>
2022-08-06 21:28:32 +0200

    Adding comments to ServiceImpl

--- src/main/java/com/idealista/application/AdsServiceImpl.java
+++ src/main/java/com/idealista/application/AdsServiceImpl.java
@@ -20,17 +20,18 @@ public class AdsServiceImpl implements AdsService {
         List<Ad> ads = adRepository.findRelevantAds();
         ads.sort(Comparator.comparing(Ad::getScore));
 
-        List<PublicAd> result = new ArrayList<>();
+        List<PublicAd> result = new ArrayList<>(); // result isn't descriptive - publicAdsList
         for (Ad ad: ads) {
             PublicAd publicAd = new PublicAd();
-            publicAd.setDescription(ad.getDescription());
+            publicAd.setDescription(ad.getDescription()); //*remove from here*
             publicAd.setGardenSize(ad.getGardenSize());
             publicAd.setHouseSize(ad.getHouseSize());
             publicAd.setId(ad.getId());
             publicAd.setPictureUrls(ad.getPictures().stream().map(Picture::getUrl).collect(Collectors.toList()));
-            publicAd.setTypology(ad.getTypology().name());
+                //obtains the URL from a list of pictures
+            publicAd.setTypology(ad.getTypology().name()); //*remove to here*
 
-            result.add(publicAd);
+            result.add(publicAd); //result.add(publicAd.createPublicAd(ad, publicAd));
         }
         return result;
     }
@@ -39,19 +40,20 @@ public class AdsServiceImpl implements AdsService {
     public List<QualityAd> findQualityAds() {
         List<Ad> ads = adRepository.findIrrelevantAds();
 
-        List<QualityAd> result = new ArrayList<>();
+        List<QualityAd> result = new ArrayList<>(); //result isn't descriptive - QualityAdsList
+        // method Create QAd
         for (Ad ad: ads) {
             QualityAd qualityAd = new QualityAd();
-            qualityAd.setDescription(ad.getDescription());
+            qualityAd.setDescription(ad.getDescription());//*remove from here*
             qualityAd.setGardenSize(ad.getGardenSize());
             qualityAd.setHouseSize(ad.getHouseSize());
             qualityAd.setId(ad.getId());
             qualityAd.setPictureUrls(ad.getPictures().stream().map(Picture::getUrl).collect(Collectors.toList()));
             qualityAd.setTypology(ad.getTypology().name());
             qualityAd.setScore(ad.getScore());
-            qualityAd.setIrrelevantSince(ad.getIrrelevantSince());
+            qualityAd.setIrrelevantSince(ad.getIrrelevantSince());//*remove to here*
 
-            result.add(qualityAd);
+            result.add(qualityAd); //result.add(qualityAd.createQualityAd(ad, qualityAd));
         }
 
         return result;
@@ -81,16 +83,17 @@ public class AdsServiceImpl implements AdsService {
         }
 
         //Calcular puntuación por descripción
-        Optional<String> optDesc = Optional.ofNullable(ad.getDescription());
+        Optional<String> optDesc = Optional.ofNullable(ad.getDescription()); //optionalDescription
 
         if (optDesc.isPresent()) {
+            //String description = Utils.cleanerDescriptions(optDesc);
             String description = optDesc.get();
 
             if (!description.isEmpty()) {
                 score += Constants.FIVE;
             }
 
-            List<String> wds = Arrays.asList(description.split(" ")); //número de palabras
+            List<String> wds = Arrays.asList(description.split(" ")); //(número de palabras) wordsUsed/wordsCount
             if (Typology.FLAT.equals(ad.getTypology())) {
                 if (wds.size() >= Constants.TWENTY && wds.size() <= Constants.FORTY_NINE) {
                    score += Constants.TEN;
@@ -106,17 +109,17 @@ public class AdsServiceImpl implements AdsService {
                     score += Constants.TWENTY;
                 }
             }
-
+            //after create de Utils.cleanerDescriptions() we can ignore de accents and uppercase in the description
             if (wds.contains("luminoso")) score += Constants.FIVE;
             if (wds.contains("nuevo")) score += Constants.FIVE;
-            if (wds.contains("céntrico")) score += Constants.FIVE;
+            if (wds.contains("céntrico")) score += Constants.FIVE; //centrico
             if (wds.contains("reformado")) score += Constants.FIVE;
-            if (wds.contains("ático")) score += Constants.FIVE;
+            if (wds.contains("ático")) score += Constants.FIVE; //atico
         }
 
         //Calcular puntuación por completitud
         if (ad.isComplete()) {
-            score = Constants.FORTY;
+            score = Constants.FORTY; //missing +
         }
 
         ad.setScore(score);
